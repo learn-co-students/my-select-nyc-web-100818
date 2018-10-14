122810c8cdd0d175f513c906a5f6ef5c3bd9f238
Henry <hanke.liu@gmail.com>
2018-10-13 22:03:53 -0400

    passed

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,24 @@
+
+
 def my_select(collection)
- # your code here!
+  i = 0
+  new_collection = []
+
+  if collection.length == 0
+    "argument is an empty array"
+  else
+    while i < collection.length
+      j = yield(collection[i])
+        if j == true
+        new_collection << collection[i]
+        end
+      i += 1
+      end
+    new_collection    
+  end
+
 end
+
+# my_select(collection) do |num|
+#   num.even?
+# end
